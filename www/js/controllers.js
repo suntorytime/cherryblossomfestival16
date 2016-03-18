b2100b946440d43843e040c18043c59f61851591
suntorytime <stevensm90@gmail.com>
2016-03-18 15:40:52 -0700

    simplify date on detail menu

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -27,6 +27,7 @@ angular.module('starter.controllers', [])
   })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
---
+++ www/js/directives.js
@@ -0,0 +1,13 @@
+angular.module('starter.directives', [])
+
+.directive('errSrc', function() {
+  return {
+    link: function(scope, element, attrs) {
+      element.bind('error', function() {
+        if (attrs.src != attrs.errSrc) {
+          attrs.$set('src', attrs.errSrc);
+        }
+      });
+    }
+  }
+});
